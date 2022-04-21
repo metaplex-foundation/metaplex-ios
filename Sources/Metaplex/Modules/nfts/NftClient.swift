f29ca6a0bc4932a77d95218daef001953af1968f
Arturo Jamaica <me@arturojamaica.com>
2022-04-21 11:00:25 -0500

    Add NFT functions

--- Sources/Metaplex/Modules/nfts/NftClient.swift
+++ Sources/Metaplex/Modules/nfts/NftClient.swift
@@ -21,4 +21,29 @@ class NftClient {
             onComplete($0)
         }
     }
+    
+    func findNftByMintList(mintKeys: [PublicKey], onComplete: @escaping (Result<[NFT?], OperationError>) -> Void) {
+        let operation = FindNftsByMintListOnChainOperationHandler(metaplex: self.metaplex)
+        operation.handle(operation: FindNftsByMintListOperation.pure(.success(
+            mintKeys
+        ))).run { onComplete($0) }
+    }
+    
+    func findNftsByCreator(creator: PublicKey, position: Int? = 1, onComplete: @escaping (Result<[NFT?], OperationError>) -> Void) {
+        let operation = FindNftsByCreatorOnChainOperationHandler(metaplex: self.metaplex)
+        operation.handle(operation: FindNftsByCreatorOperation.pure(.success(
+            FindNftsByCreatorInput(
+                creator: creator,
+                position: position
+            )))).run{ onComplete($0) }
+    }
+    
+    func findNftsByCandyMachine(candyMachine: PublicKey, version: UInt8? = 2, onComplete: @escaping (Result<[NFT?], OperationError>) -> Void) {
+        let operation = FindNftsByCandyMachineOnChainOperationHandler(metaplex: self.metaplex)
+        operation.handle(operation: FindNftsByCandyMachineOperation.pure(.success(
+            FindNftsByCandyMachineInput(
+                candyMachine: candyMachine,
+                version: version
+            )))).run{ onComplete($0) }
+    }
 }
