7f9d39b39c744560577ab45bb17150c90f8b8ac7
Arturo Jamaica <me@arturojamaica.com>
2022-05-16 16:42:37 -0500

    Add visible NFt

--- Sample/Sample/ViewController.swift
+++ Sample/Sample/ViewController.swift
@@ -9,15 +9,21 @@ import UIKit
 import Metaplex
 import Solana
 
+public class AppStorageDriver: StorageDriver { }
+
 class ViewController: UIViewController {
     
     lazy var metaplex: Metaplex = {
         let solana = SolanaConnectionDriver(endpoint: RPCEndpoint.mainnetBetaSolana)
-        return Metaplex(connection: solana, identityDriver: GuestIdentityDriver(solanaRPC: solana.solanaRPC), storageDriver: MemoryStorageDriver())
+        return Metaplex(connection: solana, identityDriver: GuestIdentityDriver(solanaRPC: solana.solanaRPC), storageDriver: AppStorageDriver())
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        metaplex.nft.findNftsByOwner(publicKey: PublicKey(string: "CN87nZuhnFdz74S9zn3bxCcd5ZxW55nwvgAv5C2Tz3K7")!) { result in
+            
+        }
         
         // Do any additional setup after loading the view.
     }
