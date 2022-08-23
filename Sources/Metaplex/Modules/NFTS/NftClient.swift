39adb704fef3ab964f13d0004ca84cc3fe233eca
Michael J. Huber Jr <mike@metaplex.com>
2022-08-23 16:46:32 -0400

    refactor: call new method from deprecated methods

--- Sources/Metaplex/Modules/NFTS/NftClient.swift
+++ Sources/Metaplex/Modules/NFTS/NftClient.swift
@@ -24,10 +24,7 @@ public class NftClient {
 
     @available(*, deprecated, renamed: "findByMint")
     public func findNftByMint(mintKey: PublicKey, onComplete: @escaping (Result<NFT, OperationError>) -> Void) {
-        let operation = FindNftByMintOnChainOperationHandler(metaplex: self.metaplex)
-        operation.handle(operation: FindNftByMintOperation.pure(.success(mintKey))).run {
-            onComplete($0)
-        }
+        findByMint(mintKey: mintKey, onComplete: onComplete)
     }
 
     public func findAllByMintList(mintKeys: [PublicKey], onComplete: @escaping (Result<[NFT?], OperationError>) -> Void) {
@@ -39,10 +36,7 @@ public class NftClient {
 
     @available(*, deprecated, renamed: "findAllByMintList")
     public func findNftByMintList(mintKeys: [PublicKey], onComplete: @escaping (Result<[NFT?], OperationError>) -> Void) {
-        let operation = FindNftsByMintListOnChainOperationHandler(metaplex: self.metaplex)
-        operation.handle(operation: FindNftsByMintListOperation.pure(.success(
-            mintKeys
-        ))).run { onComplete($0) }
+        findAllByMintList(mintKeys: mintKeys, onComplete: onComplete)
     }
 
     public func findAllByCreator(creator: PublicKey, position: Int? = 1, onComplete: @escaping (Result<[NFT?], OperationError>) -> Void) {
@@ -56,12 +50,7 @@ public class NftClient {
 
     @available(*, deprecated, renamed: "findAllByCreator")
     public func findNftsByCreator(creator: PublicKey, position: Int? = 1, onComplete: @escaping (Result<[NFT?], OperationError>) -> Void) {
-        let operation = FindNftsByCreatorOnChainOperationHandler(metaplex: self.metaplex)
-        operation.handle(operation: FindNftsByCreatorOperation.pure(.success(
-            FindNftsByCreatorInput(
-                creator: creator,
-                position: position
-            )))).run { onComplete($0) }
+        findAllByCreator(creator: creator, position: position, onComplete: onComplete)
     }
 
     public func findAllByCandyMachine(candyMachine: PublicKey, version: UInt8? = 2, onComplete: @escaping (Result<[NFT?], OperationError>) -> Void) {
@@ -75,12 +64,7 @@ public class NftClient {
 
     @available(*, deprecated, renamed: "findAllByCandyMachine")
     public func findNftsByCandyMachine(candyMachine: PublicKey, version: UInt8? = 2, onComplete: @escaping (Result<[NFT?], OperationError>) -> Void) {
-        let operation = FindNftsByCandyMachineOnChainOperationHandler(metaplex: self.metaplex)
-        operation.handle(operation: FindNftsByCandyMachineOperation.pure(.success(
-            FindNftsByCandyMachineInput(
-                candyMachine: candyMachine,
-                version: version
-            )))).run { onComplete($0) }
+        findAllByCandyMachine(candyMachine: candyMachine, version: version, onComplete: onComplete)
     }
 
     public func findAllByOwner(publicKey: PublicKey, onComplete: @escaping (Result<[NFT?], OperationError>) -> Void) {
@@ -90,7 +74,6 @@ public class NftClient {
 
     @available(*, deprecated, renamed: "findAllByOwner")
     public func findNftsByOwner(publicKey: PublicKey, onComplete: @escaping (Result<[NFT?], OperationError>) -> Void) {
-        let operation = FindNftsByOwnerOnChainOperationHandler(metaplex: self.metaplex)
-        operation.handle(operation: FindNftsByOwnerOperation.pure(.success(publicKey))).run { onComplete($0) }
+        findAllByOwner(publicKey: publicKey, onComplete: onComplete)
     }
 }
