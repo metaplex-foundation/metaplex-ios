2b85dff8b017db7fe7aec32da2f397f6dc29fd33
Arturo Jamaica <me@arturojamaica.com>
2023-02-02 14:49:32 -0600

    fix tests devnet

--- Tests/MetaplexTests/Modules/NFTs/Operations/FindNftsByOwnerOnChainOperationHandlerTests.swift
+++ Tests/MetaplexTests/Modules/NFTs/Operations/FindNftsByOwnerOnChainOperationHandlerTests.swift
@@ -14,7 +14,7 @@ final class FindNftsByOwnerOnChainOperationHandlerTests: XCTestCase {
     var metaplex: Metaplex!
     
     override func setUpWithError() throws {
-        let solanaConnection = SolanaConnectionDriver(endpoint: .mainnetBetaSolana)
+        let solanaConnection = SolanaConnectionDriver(endpoint: .devnetSolana)
         let solanaIdentityDriver = ReadOnlyIdentityDriver(solanaRPC: solanaConnection.api, publicKey: TEST_PUBLICKEY)
         let storageDriver = MemoryStorageDriver()
         metaplex = Metaplex(connection: solanaConnection, identityDriver: solanaIdentityDriver, storageDriver: storageDriver)
@@ -32,8 +32,8 @@ final class FindNftsByOwnerOnChainOperationHandlerTests: XCTestCase {
         }
         lock.run()
         let nfts = try! result?.get()
-        let nft = nfts!.first { $0!.metadataAccount.mint.base58EncodedString == "71PdnsexRQG92ZcSpEV8nn5XFqPzfK5j86yUWRF5NLyp" }!
-        XCTAssertEqual(nft?.uri, "https://arweave.net/UnB9UYQSwO3rGWSlQkwaiVDfKJOjPopV_IdWSqlLKlo")
-        XCTAssertEqual(nft?.updateAuthority.base58EncodedString, "AVgijgTG4WKDycFwYhqioMreXJpz14no8dbE8EF5roZV")
+        let nft = nfts!.first { $0!.metadataAccount.mint.base58EncodedString == "5quwyoKCi25TmamvqRDi373e1TgypCfnAKFR9h6KwB6G" }!
+        XCTAssertEqual(nft?.uri, "https://arweave.net/8G_jZ4hY-96LGxK9aK1DY2Po6NTjVbiqXM339FuzLFM")
+        XCTAssertEqual(nft?.updateAuthority.base58EncodedString, "ccc9XfyEMh9sU6DRkUmqQGJqgdKb6QyUaaT5h5BGYw4")
     }
 }
